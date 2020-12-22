316d2cf280d26798d347b42aaf66213405596535
Vladyslav Tkachenko <tkk.vlad@gmail.com>
2020-12-22 16:28:27 +0200

    First commit

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,10 @@
 require('dotenv').config();
+
+const {ApolloServer} = require('apollo-server')
+const typeDefs = require('./schema')
+
+const server = new ApolloServer({typeDefs})
+
+server.listen().then(()=>{
+    console.log('port 4000')
+})
